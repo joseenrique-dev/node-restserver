a543bb0ce34e4f6c3f63889f6ca9d43890bf921f
José Enrique López Obeso <joseenrique.dev@gmail.com>
2022-07-04 23:56:08 -0500

    add check validation for role

---
+++ models/role.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose');
+
+const RoleSchema = new Schema({
+    role: {
+        type: String,
+        required: [true, 'Role is required']
+    }
+});
+
+
+module.exports = model('Role', RoleSchema);
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const {Router} = require('express');
 const { userGet, userPut, userDelete, userPost } = require('../controller/user');
 const {check} = require('express-validator');
 const { validateField } = require('../middlewars/field-validation');
+const Role = require('../models/role');
+
 
 const router = Router();
 
@@ -12,7 +14,13 @@ const router = Router();
         check('email','Email is not valid').isEmail(),
         check('name','Name is required').not().isEmpty(),
         check('password','Password is required and greather than 6 characters').not().isEmpty().isLength({min:6}),
-        check('role','Role is required').isIn(['USER_ROLE', 'ADMIN_ROLE']),
+        // check('role','Role is required').isIn(['USER_ROLE', 'ADMIN_ROLE']),
+        check('role').custom(async (role = '') => {
+            const existRol = await Role.findOne({role});
+            if(!existRol){
+                throw new Error('Role does not exist');
+            }
+        }),
         validateField
     ], userPost);
     router.delete('/', userDelete);
