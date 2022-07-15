b353f26372b102bc2be82db9caca46b3fa314c27
José Enrique López Obeso <joseenrique.dev@gmail.com>
2022-07-15 00:05:03 -0500

    upgrade gernerate jwt fnc

--- helpers/generate-jwt.js
+++ helpers/generate-jwt.js
@@ -1,16 +1,23 @@
 const jwt = require('jsonwebtoken');
 
 const generateJWT = ( uid = '' ) =>{
-    return new Promise((resolve, reject)=>{
-        const payload = {uid};
-        jwt.sign(payload, process.env.SECRETPRIVATEKEY,{
+    return new Promise( (resolve, reject) => {
+
+        const payload = { uid };
+
+        jwt.sign( payload, process.env.SECRETPRIVATEKEY, {
             expiresIn: '4h'
-        }, (err, token)=>{
-            console.error(err)
-            if(err) reject('Something went wrong and token can not be generated');
-            resolve(token);
-        }
-    )});
+        }, ( err, token ) => {
+
+            if ( err ) {
+                console.log(err);
+                reject( 'It is not posible generate the token.' )
+            } else {
+                resolve( token );
+            }
+        })
+
+    })
 }
 
 module.exports = generateJWT;
